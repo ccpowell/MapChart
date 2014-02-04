3b6de7434b864489072f34ddb3fb3c4defdf3b18
Christopher Powell <private>
2014-02-04 13:16:40 -0700

    added barchart

---
+++ MapChart/Scripts/Site/charts-barchart.js
@@ -0,0 +1,32 @@
+﻿/*jslint devel: true, browser: true, unparam: true */
+/*global jQuery: false, app: false */
+
+app.charts.BarChart = (function ($) {
+    'use strict';
+    var template = $('#template-barchart');
+
+    function clicked(e) {
+        var command = $(this).data('command');
+        alert('clicked ' + command);
+    }
+
+    function ctor(element, options) {
+        // save options
+        this.options = $.extend(true, {}, options);
+        this.element = element;
+
+        // add HTML
+        this.element.append(template.clone());
+
+        // connect events to the element.
+        // they will be removed when the element is removed.
+        $(this.element).on('click', 'button', clicked);
+    }
+
+    ctor.prototype.destroy = function () {
+        // remove any event handlers connected to window or body
+    };
+
+    // set it
+    return ctor;
+}(jQuery));
